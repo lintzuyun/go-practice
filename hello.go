0e52199fa4bea4d6275a3a678874a0c847789cdd
Joy Lin <joy@192.168.1.198>
2022-01-12 15:52:31 +0000

    files

---
+++ hello.go
@@ -0,0 +1,21 @@
+/*
+Install Go ✅
+Tutorial - getting started ✅
+Tutorial - create a module
+*/
+
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Go())
+}
+
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
